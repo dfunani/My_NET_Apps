19ca5a2ef4aa1d11a997a1cafa240d8d06eaf94b
dfunani <dfunani@live.co.za>
2022-10-16 02:55:56 +0200

    Add project files.

---
+++ ClassLibrary/Dice/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceLibrary
+{
+    public static class Dice
+    {
+        public static int roll_dice(this int num)
+        {
+            Random randNum = new Random();
+            return randNum.Next(1, num + 1);
+
+        }
+    }
+}
---
+++ ClassLibrary/DiceRoller/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiceLibrary;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DiceLibrary.Dice.roll_dice(6).ToString());
+        }
+    }
+}
