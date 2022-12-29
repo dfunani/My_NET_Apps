548642ccc0bd942a521b7e5c7f62ed5a9e6e6be8
dfunani <dfunani@live.co.za>
2022-12-29 14:44:21 +0200

    Added phase 1 of the ToDOApp. Search bar to follow as feature for phase 2

--- ToDoApp/Form1.Designer.cs
+++ ToDoApp/Form1.Designer.cs
@@ -40,123 +40,208 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 98);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(70, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 15);
+            this.label1.Size = new System.Drawing.Size(76, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Category:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 163);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(104, 165);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.Size = new System.Drawing.Size(42, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "Task:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 216);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(54, 216);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 15);
+            this.label3.Size = new System.Drawing.Size(92, 21);
             this.label3.TabIndex = 2;
             this.label3.Text = "Description:";
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(122, 98);
+            this.textBox1.Location = new System.Drawing.Point(152, 100);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 23);
-            this.textBox1.TabIndex = 3;
+            this.textBox1.Size = new System.Drawing.Size(274, 23);
+            this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(122, 163);
+            this.textBox2.Location = new System.Drawing.Point(152, 165);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(147, 23);
-            this.textBox2.TabIndex = 4;
+            this.textBox2.Size = new System.Drawing.Size(274, 23);
+            this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(122, 216);
+            this.textBox3.Location = new System.Drawing.Point(152, 218);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(147, 122);
-            this.textBox3.TabIndex = 5;
+            this.textBox3.Size = new System.Drawing.Size(274, 184);
+            this.textBox3.TabIndex = 2;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(308, 98);
+            this.dataGridView1.Location = new System.Drawing.Point(464, 98);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(443, 240);
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(733, 367);
             this.dataGridView1.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 363);
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(152, 524);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(125, 47);
             this.button1.TabIndex = 7;
             this.button1.Text = "Refresh";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(256, 363);
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(301, 524);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(125, 47);
             this.button2.TabIndex = 8;
             this.button2.Text = "Add";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(384, 363);
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(464, 524);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(125, 47);
             this.button3.TabIndex = 9;
             this.button3.Text = "Edit";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(523, 363);
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(617, 524);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(125, 47);
             this.button4.TabIndex = 10;
             this.button4.Text = "Update";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(649, 363);
+            this.button5.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(773, 524);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(125, 47);
             this.button5.TabIndex = 11;
             this.button5.Text = "Delete";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 32F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(206, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 59);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "To0oDo0o";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "New",
+            "Started",
+            "In Progress",
+            "Overdue",
+            "Completed"});
+            this.comboBox1.Location = new System.Drawing.Point(152, 421);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(274, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(91, 419);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 21);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Status:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(925, 524);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PlaceholderText = "Search";
+            this.textBox4.Size = new System.Drawing.Size(272, 47);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1226, 635);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -170,7 +255,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Main Application Window";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -192,5 +277,9 @@
         private Button button3;
         private Button button4;
         private Button button5;
+        private Label label4;
+        private ComboBox comboBox1;
+        private Label label5;
+        private TextBox textBox4;
     }
 }
--- ToDoApp/Form1.cs
+++ ToDoApp/Form1.cs
@@ -1,18 +1,19 @@
 
+using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+
 namespace ToDoApp
 {
     public partial class Form1 : Form
     {
         DataTable table;
-        public Dictionary<String, String[]> keyValuePairs= new Dictionary<String, String[]>()
-        {
-            {"Programming_.NET", new String[]{"Programming", ".NET", "Develop a to do app using .net", "2022/07/19 15:00:00", "false" } },
-            {"Programming_Py", new String[]{"Programming", "Py", "Develop a to do app using .net", "2022/07/19 15:00:00", "false"  }},
-            {"Programming_TS", new String[]{"Programming", "TS", "Develop a to do app using .net", "2022/07/19 15:00:00", "false"  }},
-            {"Programming_C", new String[]{"Programming", "C", "Develop a to do app using .net", "2022/07/19 15:00:00", "false"  }},
-        };
+        public Dictionary<String, String[]> keyValuePairs= new Dictionary<String, String[]>(){};
         public Form1()
         {
             InitializeComponent();
@@ -26,8 +27,7 @@ namespace ToDoApp
             table.Columns.Add("Task", typeof(String));
             table.Columns.Add("Description", typeof(String));
             table.Columns.Add("Date", typeof(String));
-            table.Columns.Add("Completed", typeof(Boolean));
-
+            table.Columns.Add("Status", typeof(String));
             dataGridView1.DataSource= table;
             GetData();
         }
@@ -37,30 +37,49 @@ namespace ToDoApp
             textBox1.Clear();
             textBox2.Clear();
             textBox3.Clear();
+            comboBox1.SelectedIndex = -1;
             table.Rows.Clear();
-            foreach(KeyValuePair<String, String[]> obj in keyValuePairs)
+            try
             {
-                table.Rows.Add(obj.Value[0], obj.Value[1], obj.Value[2], obj.Value[3], obj.Value[4] == "true" ? true : false);
+                using (StreamReader file = File.OpenText(@".\test.json"))
+                {
+                    Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                    keyValuePairs = (Dictionary<string, string[]>)serializer.Deserialize(file, typeof(Dictionary<String, String[]>));
+                }
+            }
+            catch(Exception ex) { Console.WriteLine(""); }
+            foreach (KeyValuePair<String, String[]> obj in keyValuePairs)
+            {
+                table.Rows.Add(obj.Value[0], obj.Value[1], obj.Value[2], obj.Value[3], obj.Value[4]);
             }
 
         }
         public void SaveData() 
         {
-            if(textBox1.Text != "" && textBox2.Text != "")
+            if (textBox1.Text != "" && textBox2.Text != "")
             {
-                try
+                using(StreamWriter file = File.CreateText(@".\test.json"))
                 {
-                    keyValuePairs.Add(textBox1.Text + "_"+ textBox2.Text, new String[] { textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString(), "false" });
+                    Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                    keyValuePairs[textBox1.Text + "_" + textBox2.Text] = new string[] { textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString(), comboBox1.Text };
+                    serializer.Serialize(file, keyValuePairs);
                 }
-                catch 
-                {
-                    keyValuePairs.Remove(textBox1.Text + "_" + textBox2.Text);
-                    keyValuePairs.Add(textBox1.Text + "_"+ textBox2.Text, new String[] { textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString(), "false" });
-
-                }
+                GetData();
             }
         }
-
+        public void DeleteData()
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                using (StreamWriter file = File.CreateText(@".\test.json"))
+                {
+                    Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                    keyValuePairs[textBox1.Text + "_" + textBox2.Text] = new string[] { textBox1.Text, textBox2.Text, textBox3.Text, DateTime.Now.ToString(), comboBox1.Text };
+                    serializer.Serialize(file, keyValuePairs);
+                }
+                GetData();
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             GetData();
@@ -85,10 +104,10 @@ namespace ToDoApp
 
         private void button4_Click(object sender, EventArgs e)
         {
-            int index = dataGridView1.CurrentCell.RowIndex;
 
             try
             {
+                int index = dataGridView1.CurrentCell.RowIndex;
                 keyValuePairs.Remove(table.Rows[index]["Category"].ToString() + "_" + table.Rows[index]["Task"].ToString());
                 keyValuePairs.Add(table.Rows[index]["Category"].ToString() + "_" + table.Rows[index]["Task"].ToString(), new String[] {
                 table.Rows[index]["Category"].ToString(), table.Rows[index]["Task"].ToString(),
@@ -107,7 +126,22 @@ namespace ToDoApp
             textBox1.Text = table.Rows[index]["Category"].ToString();
             textBox2.Text = table.Rows[index]["Task"].ToString();
             textBox3.Text = table.Rows[index]["Description"].ToString();
+            comboBox1.Text = table.Rows[index]["Status"].ToString();
+        }
 
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            List<String> res = new List<String>();
+            string temp = "";
+            Regex regex = new Regex(textBox4.Text);
+            foreach(string key in keyValuePairs.Keys)
+            {
+                if(regex.IsMatch(key)) {
+                    res.Add(key);
+                    temp += key;
+                }
+            }
+            MessageBox.Show(temp);
         }
     }
 }
