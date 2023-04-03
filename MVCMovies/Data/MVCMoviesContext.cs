e3279f808b7e528629ee52dc78590071de14dcb7
dfunani <dfunani@live.co.za>
2023-04-04 00:11:30 +0200

    Added MVC

---
+++ MVCMovies/Data/MVCMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMovies.Models;
+
+namespace MVCMovies.Data
+{
+    public class MVCMoviesContext : DbContext
+    {
+        public MVCMoviesContext (DbContextOptions<MVCMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCMovies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MVCMovies/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } = default(int);
+        public string? Title { get; set; } = default(string);
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; } = default;
+        public string? Genre{ get; set; } = default;
+        public decimal Price { get; set; } = default;
+    }
+}
