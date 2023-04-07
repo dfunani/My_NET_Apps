937b4f503de669090eaf52225fc13505ce6530f6
dfunani <dfunani@live.co.za>
2023-04-07 03:42:44 +0200

    done

---
+++ WebApiMovies/Data/WebApiItemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiMovies.Models;
+
+namespace WebApiMovies.Data
+{
+    public class WebApiItemContext : DbContext
+    {
+        public WebApiItemContext (DbContextOptions<WebApiItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiMovies.Models.Item> Item { get; set; } = default!;
+    }
+}
---
+++ WebApiMovies/Migrations/20230407013203_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+        }
+    }
+}
