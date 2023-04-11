ffc0e89dab3136f01e3f09175ad0c361a0b10727
dfunani <dfunani@live.co.za>
2023-04-11 13:29:33 +0200

    Created tuts

--- VSCodeConsole/Program.cs
+++ VSCodeConsole/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int res = 0;
+		int.TryParse("0250", out res);
+		Console.WriteLine(res);
+		// See https://aka.ms/new-console-template for more information
+		/*Console.WriteLine("Quantities:");
+		HumanizeQuantities();
+
+		Console.WriteLine("\nDate/Time Manipulation:");
+		HumanizeDates(); */
+
+		/* IEnumerable<string> dir = Directory.EnumerateFiles(".", "*.cs*", SearchOption.AllDirectories);
+		foreach (string file in dir)
+		{
+			Console.WriteLine(file);
+		}
+		 */
+		Console.WriteLine(Environment.CommandLine);
+	}
+
+	static void HumanizeQuantities()
+	{
+		Console.WriteLine("case".ToQuantity(0));
+		Console.WriteLine("case".ToQuantity(1));
+		Console.WriteLine("case".ToQuantity(5));
+	}
+
+	static void HumanizeDates()
+	{
+		Console.WriteLine(DateTime.UtcNow.AddHours(-48).Humanize());
+		Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+		Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+		Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+	}
+}
